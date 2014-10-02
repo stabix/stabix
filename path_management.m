67e3e07521e4a1e1f772d459a8afd700c75b6a93
CZ <CZ>
2014-10-02 14:17:54 +0200

    you can now do "path_management y" to setup the stabix environment

--- path_management.m
+++ path_management.m
@@ -1,5 +1,5 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-function path_management
+function path_management(varargin)
 %% Set Matlab search path
 % http://www.mathworks.de/de/help/matlab/ref/addpath.html
 commandwindow;
@@ -7,19 +7,36 @@ commandwindow;
 S = dbstack('-completenames');
 [folder, name, ext] = fileparts(S(1).file);
 display (folder);
-answer = input('Add the above folder with subfolders to the MATLAB search path ?\n ([y](default)/n/rm(remove))','s');
-path_to_add = genpath(folder);
+
+if nargin > 0 && ischar(varargin{1})
+    answer = varargin{1};
+else
+    answer = input('Add the above folder with subfolders to the MATLAB search path ?\n ([y](default)/n/rm(remove))','s');
+end
+
+path_to_add = genpath(folder); 
+% TODO: gemove everything under .git/...
+% this will be much easier with strsplit, available from matlab2013a (8.1)
+path_cell = regexp(path_to_add, pathsep, 'split');
+%try
+path_cell_genpath = path_cell;
+path_cell = cellstr_filter(path_cell, {'.git'});
+filtered_entries = numel(path_cell_genpath) - numel(path_cell)
+
+n_dirs = numel(path_cell);
+
+path_to_add = cell2path(path_cell);
 
 if strcmpi(answer, 'y') || isempty(answer)
-    display 'adding it';
+    display(sprintf('Adding %i entries to matlab search path', n_dirs));
     addpath(path_to_add);
     %rmpath(path_to_ignore)
     %savepath;
-    setenv('SLIP_TRANSFER_TBX_ROOT', folder)
+    setenv('SLIP_TRANSFER_TBX_ROOT', folder) 
 elseif strcmpi(answer, 'rm')
-    display 'removing it';
+    display(sprintf('Removing %i entries from matlab search path', n_dirs));
     rmpath(path_to_add);
-    setenv('SLIP_TRANSFER_TBX_ROOT', '')
+    setenv('SLIP_TRANSFER_TBX_ROOT', '') % delete environment variable, TODO: works on Linux?
 else
     display 'doing nothing';
 end
