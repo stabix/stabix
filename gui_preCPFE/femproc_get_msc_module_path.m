5f676e3baa883ec5ca538855a895a772102012a6
david mercier <d.mercier@mpie.de>
2014-10-17 20:25:57 +0200

    Remove unused function and update YAML config file.

--- gui_preCPFE/femproc_get_msc_module_path.m
+++
@@ -1,9 +0,0 @@
-% Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-function msc_module_path = femproc_get_msc_module_path
-%% Function used to get the "msc module" path
-% author: d.mercier@mpie.de
-
-msc_module_path = fullfile(get_stabix_root, 'third_party_code', 'python', 'msc');
-
-
-end
