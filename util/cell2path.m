9fe395e5d92dc7b38d2c2e140eedb22756a17a3b
CZ <CZ>
2014-10-20 18:23:55 +0200

    added utility functions to manipulate the matlab search path

---
+++ util/cell2path.m
@@ -0,0 +1,14 @@
+function pstr = path2cell(p_cstr)
+%joins a matlab search path cell string with the pathseparator and returns the
+%result as a STR
+
+if ~iscellstr(p_cstr)
+    error('Only accepts CELLSTR')
+end
+
+pstr = '';
+
+for item_idx = 1:numel(p_cstr)
+    item = p_cstr(item_idx);
+    pstr = [pstr, pathsep, item{1}];
+end
---
+++ util/cellstr_filter.m
@@ -0,0 +1,28 @@
+function cellstr_out = cellstr_filter(cellstring, filter_cell_or_str)
+% Delete all entries of a cell array of string that math the filter
+% filter can be string or cellstr
+% maybe could be implemented faster by using "regexprep", TODO...
+if ~ iscellstr(filter_cell_or_str)
+    if isstr(filter_cell_or_str)
+        filter_cell = {filter_cell_or_str};
+    else
+        error('takes only CELLSTRs or STRs as filter');
+    end
+else
+    filter_cell = filter_cell_or_str;
+end
+cellstr_out = {};
+for el_idx = 1:numel(cellstring);
+    stritem = cellstring(el_idx);
+    for fstr_idx = 1:numel(filter_cell)
+        fstr = filter_cell(fstr_idx);
+        if strfind(stritem{1}, fstr{1})
+            %stritem{1}
+            continue % don't add filtered elements
+        else
+            cellstr_out{end+1} = stritem{1};
+        end
+    end
+end
+%filtered_ = numel(cellstring) - numel(cellstr_out)
+            
