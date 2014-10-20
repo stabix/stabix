0d67eb2b4279b8af2ea128ef42f23e51c0a54420
david mercier <d.mercier@mpie.de>
2014-10-20 17:57:40 +0200

    Changed syntax (strcmp(strtok)) into strfind. Changed default path for Python pakage 4 CPFEM.

--- gui_preCPFE/femproc_get_python4fem_module_path.m
+++ gui_preCPFE/femproc_get_python4fem_module_path.m
@@ -3,9 +3,6 @@ function python4fem_module_path = femproc_get_python4fem_module_path
 %% Function used to get the "python4fem module" path
 % author: d.mercier@mpie.de
 
-% FIXME ==> not the same paths in GitHub and MWN2 repos
 python4fem_module_path = fullfile(get_stabix_root, 'third_party_code', 'python');
-
-%python4fem_module_path = fullfile(fileparts(get_stabix_root), 'python'); % Only for MWN2 repo
 
 end
