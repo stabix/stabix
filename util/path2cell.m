4c403860049e66b2bcec65ba278ae3257133305d
unknown <d.mercier@mapc103.mpie.de>
2014-10-23 14:36:00 +0200

    add copyright and remove fieldname unused in YAML config file

--- util/path2cell.m
+++ util/path2cell.m
@@ -1,4 +1,5 @@
+% Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
 function pcell = path2cell(path_str)
-%splits a matlab search path string at the pathseparator and returns the
+%% Splits a Matlab search path string at the pathseparator and returns the
 %result as a cellstr
 pcell = regexp(path_str, pathsep, 'split');
