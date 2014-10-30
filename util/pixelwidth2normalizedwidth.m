766b06147092e0d853f30d549b8925b9951db6f2
unknown <d.mercier@mapc103.mpie.de>
2014-10-30 18:19:00 +0100

    updated demo.m and add function to normalize width of a gui.

---
+++ util/pixelwidth2normalizedwidth.m
@@ -0,0 +1,13 @@
+% Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
+function nw = pixelwidth2normalizedwidth(pw)
+%% Normalized a width in pixels
+% pw: width in pixels
+% nw: normalized width
+
+scr = get(0);    % Get screen size  
+scr.width = scr.ScreenSize(3);
+
+nw = pw / scr.width;
+
+end
+
