stabix/stabix

commit 9253a8c16d2919e4d0876aa31e1a021649927d73

Improved/Cleaned functions in preCPFE package

gui_preCPFE/preCPFE_python_select.m
@@ -1,18 +1,18 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-function python_executable = preCPFE_python_select
+function [python_executable, python_checked] = ...
+    preCPFE_python_select(handle_pm_Python)
 %% Get Python installation selected
+% handle_pm_Python: Handle of the popup menu for Python executables
 
-gui = guidata(gcf);
+% author: c.zambaldi@mpie.de
 
-python_executable_list = get(gui.handles.pm_Python, 'String');
-python_executable_selected = get(gui.handles.pm_Python, 'Value');
+python_executable_list = get(handle_pm_Python, 'String');
+python_executable_selected = get(handle_pm_Python, 'Value');
 
 python_executable = python_executable_list(python_executable_selected);
 python_executable = python_executable{:};
 
-gui.config.CPFEM.python = python_check(python_executable);
-
-guidata(gcf, gui);
+python_checked = python_check(python_executable);
 
 preCPFE_config_CPFEM_updated;
 

gui_preCPFE/A_preCPFE_windows_indentation_setting_BX.m
@@ -1,5 +1,6 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-function gui_handle = A_preCPFE_windows_indentation_setting_BX(gui_bicrystal, varargin)
+function gui_handle = ...
+    A_preCPFE_windows_indentation_setting_BX(gui_bicrystal, varargin)
 %% Setting of indentation inputs (tip radius, indentation depth...) 
 % and setting of the mesh for a bicrystal indentation experiment.
 
@@ -83,9 +84,10 @@ gui_BX.handles.indenter = preCPFE_buttons_indenter(x0, hu, wu);
 %% Creation of buttons/popup menus (mesh quality, layout, Python, CPFEM...)
 gui_BX.handles.other_setting = preCPFE_buttons_gui(x0, hu, wu);
 
-%% Set GUI handle encapsulation
+%% Set Python executable
 guidata(gcf, gui_BX);
-gui_BX.config.CPFEM.python_executable = preCPFE_python_select;
+[gui_BX.config.CPFEM.python_executable, gui_BX.config.CPFEM.python] = ...
+    preCPFE_python_select(gui_BX.handles.pm_Python);
 guidata(gcf, gui_BX);
 
 %% Run the plot of the meshing

gui_preCPFE/A_preCPFE_windows_indentation_setting_SX.m
@@ -1,5 +1,6 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-function gui_handle = A_preCPFE_windows_indentation_setting_SX(gui_bicrystal, activeGrain, varargin)
+function gui_handle = ...
+    A_preCPFE_windows_indentation_setting_SX(gui_bicrystal, activeGrain, varargin)
 %% Setting of indentation inputs (tip radius, indentation depth...)
 % and setting of the mesh for a single crystal indentation experiment.
 
@@ -92,9 +93,10 @@ gui_SX.handles.other_setting = preCPFE_buttons_gui(x0, hu, wu);
 set(gui_SX.handles.other_setting.pm_mesh_color_title, 'Visible', 'off');
 set(gui_SX.handles.other_setting.pm_mesh_color, 'Visible', 'off');
 
-%% Set GUI handle encapsulation
+%% Set Python executable
 guidata(gcf, gui_SX);
-gui_SX.config.CPFEM.python_executable = preCPFE_python_select;
+[gui_SX.config.CPFEM.python_executable, gui_SX.config.CPFEM.python] = ...
+    preCPFE_python_select(gui_SX.handles.pm_Python);
 guidata(gcf, gui_SX);
 
 %% Run the plot of the meshing

gui_preCPFE/preCPFE_3d_conospherical_indenter.m
@@ -1,5 +1,6 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-function handle_indenter = preCPFE_3d_conospherical_indenter(tipRadius, coneAngle, N, X_position, Y_position, Z_position, varargin)
+function handle_indenter = preCPFE_3d_conospherical_indenter(...
+    tipRadius, coneAngle, N, X_position, Y_position, Z_position, varargin)
 %% Function to plot a 3D conospherical indenter
 % tipRadius : Radius of the spherical part of the indenter.
 % coneAngle : Full angle in degree of the conical part of the indenter.

gui_preCPFE/preCPFE_3d_flat_punch_indenter.m
@@ -1,5 +1,6 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-function handle_indenter = preCPFE_3d_flat_punch_indenter(tipRadius, X_position, Y_position, Z_position, szFac, varargin)
+function handle_indenter = preCPFE_3d_flat_punch_indenter(...
+    tipRadius, X_position, Y_position, Z_position, szFac, varargin)
 %% Function to plot a 3D flat punch indenter
 % tipRadius : Radius of the flat punch indenter.
 % szFac: Factor to scale the 3D plot.

gui_preCPFE/preCPFE_3d_indenter_topo_AFM.m
@@ -1,5 +1,7 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-function [X, Y, data, fvc, handle_indenter] = preCPFE_3d_indenter_topo_AFM(topo, h_indent, smooth_factor, z_factor, varargin)
+function [X, Y, data, fvc, handle_indenter] = ...
+    preCPFE_3d_indenter_topo_AFM(...
+    topo, h_indent, smooth_factor, z_factor, varargin)
 %% Function to correct topography from Gwyddion file
 % topo: Struct. variable imported from Gwyddion file (see
 % read_gwyddion_ascii function)
@@ -66,15 +68,18 @@ topo.Y = topo.Y - yShift;
 if smooth_flag
     for ii = 1:1:topo.nX/smooth_factor
         for jj = 1:1:topo.nY/smooth_factor
-            topo.X_smoothed(ii,jj) = mean([topo.X(smooth_factor*ii-1,2*jj-1),...
+            topo.X_smoothed(ii,jj) = ...
+                mean([topo.X(smooth_factor*ii-1,2*jj-1),...
                 topo.X(smooth_factor*ii-1,smooth_factor*jj),...
                 topo.X(smooth_factor*ii,smooth_factor*jj-1),...
                 topo.X(smooth_factor*ii,smooth_factor*jj)]);
-            topo.Y_smoothed(ii,jj) = mean([topo.Y(smooth_factor*ii-1,2*jj-1),...
+            topo.Y_smoothed(ii,jj) = ...
+                mean([topo.Y(smooth_factor*ii-1,2*jj-1),...
                 topo.Y(smooth_factor*ii-1,smooth_factor*jj),...
                 topo.Y(smooth_factor*ii,smooth_factor*jj-1),...
                 topo.Y(smooth_factor*ii,smooth_factor*jj)]);
-            topo.data_smoothed(ii,jj) = mean([topo.data(smooth_factor*ii-1,2*jj-1),...
+            topo.data_smoothed(ii,jj) = ...
+                mean([topo.data(smooth_factor*ii-1,2*jj-1),...
                 topo.data(smooth_factor*ii-1,smooth_factor*jj),...
                 topo.data(smooth_factor*ii,smooth_factor*jj-1),...
                 topo.data(smooth_factor*ii,smooth_factor*jj)]);
@@ -82,7 +87,8 @@ if smooth_flag
     end
     
     %% Inverse and Z-shift data (because of AFM measurement)
-    topo.data_smoothed = (-(topo.data_smoothed - max(max(topo.data_smoothed)))) - h_indent;
+    topo.data_smoothed = ...
+        (-(topo.data_smoothed - max(max(topo.data_smoothed)))) - h_indent;
 else
     topo.X_smoothed = topo.X;
     topo.Y_smoothed = topo.Y;

gui_preCPFE/preCPFE_3d_polygon_indenter.m
@@ -1,5 +1,6 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-function [handle_indenter, ptc] = preCPFE_3d_polygon_indenter(N, ang, height, zShift, varargin)
+function [handle_indenter, ptc] = preCPFE_3d_polygon_indenter(...
+    N, ang, height, zShift, varargin)
 %% Function to plot a 3D polygon indenter
 % N: Number of sides of the polygon (e.g.: N = 3 ==> three-sided indenter,
 % N = 4 ==> four-sided indenter). N > 2
@@ -48,6 +49,7 @@ deg_inc = 360/N;
 vts(1,:) = [0,0,0];
 sz = tand(ang) / cosd(deg_inc/2);
 
+fcs = zeros(N+1, 4);
 for iN = 1:N+1
     vts(iN+1, :) = [cosd(iN * deg_inc) * sz, ...
         sind(iN * deg_inc) * sz, 1] * height;

gui_preCPFE/preCPFE_bias.m
@@ -1,5 +1,6 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-function biased_elem = preCPFE_bias(fem_software, x0, xN, num_elements, bias)
+function biased_elem = ...
+    preCPFE_bias(fem_software, x0, xN, num_elements, bias)
 %% Function used to select bais function to use (Mentat or Abaqus)
 % fem_software: mentat or abaqus
 % x0: First point

commit dff238f30480d0373fd8ca7cc108730e69407192

fixed regression for python check introduced in 9253a8c16d2919e4d0876aa31e1

gui_preCPFE/preCPFE_python_select.m
@@ -6,6 +6,8 @@ function [python_executable, python_checked] = ...
 
 % author: c.zambaldi@mpie.de
 
+gui = guidata(gcf);
+
 python_executable_list = get(handle_pm_Python, 'String');
 python_executable_selected = get(handle_pm_Python, 'Value');
 
@@ -13,6 +15,9 @@ python_executable = python_executable_list(python_executable_selected);
 python_executable = python_executable{:};
 
 python_checked = python_check(python_executable);
+
+gui.config.CPFEM.python = python_checked;
+guidata(gcf, gui);
 
 preCPFE_config_CPFEM_updated;
 