ab21c0fc7fb861cd6857bd8eae07f40f7b1b73db
unknown <d.mercier@mapc103.mpie.de>
2014-11-20 17:43:58 +0100

    Updated list of lattice constants.

--- crystallo/latt_param.m
+++ crystallo/latt_param.m
@@ -7,11 +7,14 @@ function lattice_parameters = latt_param(material, structure, varargin)
 % structure : structure of the given material (hcp, bcc, fcc, dia, tet or bct)
 % author: d.mercier@mpie.de
 
+% [DAVEY 1925] DOI ==> 10.1103/PhysRev.25.753
+% [SPEDDING 1956] DOI ==> 10.1107/S0365110X5600156X
 % [RAYNE 1960] : DOI ==> 10.1103/PhysRev.120.1658
 % [ASHCROFT 1976] : Ashcroft and Mermin - "Solid State Physics"
 % [YOO 1981] : DOI ==> 10.1007/BF02648537
 % [LANDOLT-BORNSTEIN 2002] : DOI ==> 10.1007/10832182_543
-% [BATTAINI 2008] : PhD thesis of Battaini M. - "Deformation behaviour and twinning mechanisms of commercially pure titanium alloys"
+% [BATTAINI 2008] : PhD thesis of Battaini M. -
+% "Deformation behaviour and twinning mechanisms of commercially pure titanium alloys"
 % [SEAL 2012] : http://dx.doi.org/10.1016/j.msea.2012.04.114
 
 % Initialization
@@ -48,6 +51,8 @@ if ~flag_error
             c = 5.06; a = 3.20;                                            % from [ASHCROFT 1976]
         elseif strcmp(material,'Ho') == 1
             c = 5.62; a = 3.58;                                            % from [ASHCROFT 1976]
+        elseif strcmp(material,'La') == 1
+            c = 12.159; a = 3.770;                                         % from [SPEDDING 1956]
         elseif strcmp(material,'Lu') == 1                                  % || strcmp(material,'alpha-Lu') == 1
             c = 5.55; a = 3.50;                                            % from [ASHCROFT 1976]
         elseif strcmp(material,'Mg') == 1
@@ -68,6 +73,8 @@ if ~flag_error
             c = 4.683; a = 2.950;                                          % from [BATTAINI 2008] - NB: [ASHCROFT 1976] c = 4.69 ; a = 2.95;  NB:[YOO 1981] c/a(Ti)= 1.588
         elseif strcmp(material,'Tl') == 1                                  % || strcmp(material,'alpha-Tl') == 1
             c = 5.53; a = 3.46;                                            % from [ASHCROFT 1976]
+        elseif strcmp(material,'Tm') == 1
+            c = 5.5546; a = 3.5375;                                        % from [SPEDDING 1956]
         elseif strcmp(material,'Y') == 1
             c = 5.73; a = 3.65;                                            % from [ASHCROFT 1976]
         elseif strcmp(material,'Zn') == 1
@@ -85,6 +92,8 @@ if ~flag_error
             a = 2.88; c = a;                                               % from [ASHCROFT 1976]
         elseif strcmp(material,'Cs') == 1
             a = 6.05; c = a;                                               % from [ASHCROFT 1976] @ 78K
+        elseif strcmp(material,'Eu') == 1
+            a = 4.606; c = a;                                              % from [SPEDDING 1956]
         elseif strcmp(material,'Fe') == 1                                  % || strcmp(material,'alpha-Fe') == 1
             a = 2.87; c = a;                                               % from [ASHCROFT 1976] (alpha-Ferrite)
         elseif strcmp(material,'K') == 1
@@ -144,6 +153,8 @@ if ~flag_error
             a = 3.80; c = a;                                               % from [ASHCROFT 1976]
         elseif strcmp(material,'Sr') == 1                                  % || strcmp(material,'alpha-Sr') == 1
             a = 6.08; c = a;                                               % from [ASHCROFT 1976]
+        elseif strcmp(material,'Yb') == 1
+            a = 5.4862; c = a;                                             % from [SPEDDING 1956]
         else
             a = 1; c = 0;
         end
@@ -169,6 +180,11 @@ if ~flag_error
         
     elseif strcmp(material,'bct') == 1
         c = 0; a = 1;
+        
+    elseif strcmp(material,'tri') == 1
+        if strcmp(material,'Bi') == 1
+            c = 3.1815; a = 5.8316;                                        % from [DAVEY 1925]
+        end
         
     else
         c = 0; a = 1;
