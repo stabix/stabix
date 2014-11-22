d747758fbf89f893b1a03210931f400975623fe7
David MERCIER <d.mercier@mpie.de>
2014-11-22 14:14:05 +0100

    improved function to calibrate scale in GBinc toolbox

--- gui_gbinc/gui_gbinc_scale_calibration.m
+++ gui_gbinc/gui_gbinc_scale_calibration.m
@@ -15,12 +15,20 @@ if image_type == 1
         gui.config_map.scale_str = ...
             get(gui.handles.image_before_polishing.scale_value, 'string');
         gui.config_map.scale = str2double(gui.config_map.scale_str);
-        gui.config_map.dist = getDistance(gui.config_map.h_dist);
-        gui.config_map.factor_calib_1 = ...
-            gui.config_map.scale / gui.config_map.dist;
-        set(gui.handles.image_before_polishing.scale_factor_value, ...
-            'string', gui.config_map.factor_calib_1);
-        gui.flag.calibration_1 = 1;
+        try
+            gui.config_map.dist = getDistance(gui.config_map.h_dist);
+            gui.config_map.factor_calib_1 = ...
+                gui.config_map.scale / gui.config_map.dist;
+            set(gui.handles.image_before_polishing.scale_factor_value, ...
+                'string', gui.config_map.factor_calib_1);
+            gui.flag.calibration_1 = 1;
+        catch
+            gui.config_map.dist = NaN;
+            gui.config_map.factor_calib_1 = NaN;
+            set(gui.handles.image_before_polishing.scale_factor_value, ...
+                'string', gui.config_map.factor_calib_1);
+            gui.flag.calibration_1 = 0;
+        end
     end
 elseif image_type == 2
     if ~gui.flag.image2
@@ -29,12 +37,20 @@ elseif image_type == 2
         gui.config_map.scale_str = ...
             get(gui.handles.image_after_polishing.scale_value, 'string');
         gui.config_map.scale = str2double(gui.config_map.scale_str);
-        gui.config_map.dist = getDistance(gui.config_map.h_dist);
-        gui.config_map.factor_calib_2 = ...
-            gui.config_map.scale / gui.config_map.dist;
-        set(gui.handles.image_after_polishing.scale_factor_value, ...
-            'string', gui.config_map.factor_calib_2);
-        gui.flag.calibration_2 = 1;
+        try
+            gui.config_map.dist = getDistance(gui.config_map.h_dist);
+            gui.config_map.factor_calib_2 = ...
+                gui.config_map.scale / gui.config_map.dist;
+            set(gui.handles.image_after_polishing.scale_factor_value, ...
+                'string', gui.config_map.factor_calib_2);
+            gui.flag.calibration_2 = 1;
+        catch
+            gui.config_map.dist = NaN;
+            gui.config_map.factor_calib_2 = NaN;
+            set(gui.handles.image_after_polishing.scale_factor_value, ...
+                'string', gui.config_map.factor_calib_2);
+            gui.flag.calibration_2 = 0;
+        end
     end
 end
 
