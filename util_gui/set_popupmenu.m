1748db6bdfbeae10859ab7c8c717c20d2f072cb2
unknown <d.mercier@mapc103.mpie.de>
2014-11-28 12:20:13 +0100

    added function to set popupmenu and function to define list of coordinate systems

---
+++ util_gui/set_popupmenu.m
@@ -0,0 +1,49 @@
+% Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
+function [string, popupmenu] = ...
+    set_popupmenu(str, pos, val, listpm, callback, varargin)
+%% Function used to create automatically a serir of txt boxes + editable txt boxes in the GUI
+% str : string for the txt box
+% pos : position of the txt box
+% val : value of the variable to edit in corresponding editable txt box
+% listpm : list to fill the popupmenu
+% callback : callback function or script to assess to the editable txt box
+
+% authors: d.mercier@mpie.de
+
+if nargin < 4
+    callback = '';
+end
+
+if nargin < 3
+    val = '';
+end
+
+if nargin < 2
+    pos = [0.9 0.9 0.9];
+end
+   
+if nargin < 1
+    str = 'test';
+end
+
+parent = gcf;
+
+string = uicontrol(...
+    'Parent', parent,...
+    'Units', 'normalized',...
+    'Style', 'text',...
+    'HorizontalAlignment', 'left',...
+    'Position', pos,...
+    'String', str);
+
+popupmenu = uicontrol(...
+    'Parent', parent,...
+    'Units', 'normalized',...
+    'Style', 'popupmenu',...
+    'Position', [pos(1) pos(2)-0.02 pos(3) pos(4)], ...
+    'BackgroundColor', [0.9 0.9 0.9],...
+    'Value', val,...
+    'String', listpm,...
+    'Callback', callback);
+
+end
