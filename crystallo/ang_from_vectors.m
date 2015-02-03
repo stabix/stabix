ef46aa67e2e85100e21609290695f7726fbc1e50
unknown <d.mercier@mapc103.mpie.de>
2015-02-03 10:24:47 +0100

    added function to calculate angle between vectors

---
+++ crystallo/ang_from_vectors.m
@@ -0,0 +1,9 @@
+% Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
+function angle_from_cos = ang_from_vectors(v1, v2)
+%% Return the angle in degress from the dot product between the 2 vectors (v1 and v2)
+
+cosine = cos_from_vectors(v1, v2);
+
+angle_from_cos = acosd(cosine);
+
+return
