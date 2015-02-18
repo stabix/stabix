056741dd227279a16fd2eb5931975abea57773c2
unknown <d.mercier@mapc103.mpie.de>
2015-02-18 20:04:39 +0100

    added function to convert EBSD data loaded using MTEX import_wizard in TSL format (GF2 and RB files)

---
+++ MTEX_functions/MTEX_convert2TSLdata.m
@@ -0,0 +1,69 @@
+% Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
+function [grains, obj, fdata] = ...
+    MTEX_convert2TSLdata(ebsd, angle_value, varargin)
+%% Function used to convert data loaded via the import_wizard of MTEX
+% into TSL format files (GF2 and RB files)
+% ebsd: MTEX structure variable
+% angle_value: A grain is defined as a region, in which the misorientation
+% of neighbored measurements is less than the given angle.
+
+% See in http://mtex-toolbox.github.io/
+
+% author: d.mercier@mpie.de
+
+if nargin < 2
+    angle_value = 5;
+end
+
+if nargin < 1
+    import_wizard;
+end
+
+%% Grains definition
+grains = calcGrains(ebsd, 'angle', angle_value * degree);
+% subSet function to get only 1 grain
+
+% grains = smooth(grains,iter,..,param,val,..);
+
+%% Initialization of path
+fpath = get_stabix_root;
+
+%% Grain File type 2
+fname_GF2 = 'testGF2.txt';
+
+fdata.title = 'Data from MTEX';
+fdata.number_of_grains = grains.size(1);
+fdata.eul_ang = (grains.meanOrientation.Euler) * 180/pi;
+grain_position = grains.centroid;
+fdata.x_positions = grain_position(:,1);
+fdata.y_positions = grain_position(:,2);
+fdata.phase = nonzeros(grains.phaseId);
+fdata.grain_diameter = ((grains.area/pi).^0.5)./2;
+
+% Write TSL-OIM grain file type 2
+write_oim_grain_file_type2(fdata, fpath, fname_GF2);
+
+%% Reconstructed Boundaries file
+fname_RB = 'testRB.txt';
+
+%p = patch('Faces',grains.boundary.F,'Vertices',grains.boundary.V,'FaceColor','w');
+%fdata.number_of_grain_boundaries = size(grains.boundary.F,1);
+
+[grains_x, grains_y] = centroid(grains);
+
+[fdata.GBvx, ...
+    fdata.GBvy, ...
+    fdata.GB2cells, ...
+    fdata.flag_VorFailed] = ...
+    neighbooring_edge_of_2cells(...
+    grains_x', grains_y');
+
+fdata.GBvx = fdata.GBvx';
+fdata.GBvy = fdata.GBvy';
+fdata.GB2cells = fdata.GB2cells';
+fdata.number_of_grain_boundaries = size(fdata.GBvx, 1);
+
+% Write TSL-OIM grain file type 2
+write_oim_reconstructed_boundaries_file(fdata, fpath, fname_RB);
+
+end
