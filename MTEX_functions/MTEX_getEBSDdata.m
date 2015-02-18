5d33bde6c482d72d684da357fc17a2189f07d795
unknown <d.mercier@mapc103.mpie.de>
2015-02-18 20:03:15 +0100

    update function to load EBSD data using MTEX import_wizard

--- MTEX_functions/MTEX_getEBSDdata.m
+++ MTEX_functions/MTEX_getEBSDdata.m
@@ -1,17 +1,31 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
 function MTEX_getEBSDdata
 %% Import data with MTEX toolbox
-% See in http://code.google.com/p/mtex/
+% See in http://mtex-toolbox.github.io/
 
 % authors: d.mercier@mpie.de / c.zambaldi@mpie.de
 
 try
-    import_wizard('PoleFigure');
+    import_wizard('EBSD');
     
-catch err 
+    MTEX_env = getMTEXpref;
+    
+    if exist('ebsd', 'var')
+        MTEX_convert2TSLdata(ebsd);
+        
+        CoordSysVal = MTEX_setCoordSys(MTEX_env.xAxisDirection, ...
+            MTEX_env.zAxisDirection);
+        set(gui.handles.pmcoordsyst, 'Value', CoordSysVal);
+    else
+        commandwindow;
+        display(['Reload your EBSD data and save it as a variable ', ...
+            'named ''ebsd'' in the Matlab workspace']);
+    end
+    
+catch err
     commandwindow;
     display(err.message);
-
+    
 end
 
 end
