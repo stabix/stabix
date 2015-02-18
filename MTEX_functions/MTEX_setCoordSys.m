e368008c574ac3267502d32fb2d2331a25f10bcf
unknown <d.mercier@mapc103.mpie.de>
2015-02-18 20:05:13 +0100

    function to convert coord. system from  MTEX to STABiX

---
+++ MTEX_functions/MTEX_setCoordSys.m
@@ -0,0 +1,26 @@
+% Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
+function CoordSysVal = MTEX_setCoordSys(xAxisDirection, zAxisDirection)
+%% Import data with MTEX toolbox
+% See in http://mtex-toolbox.github.io/
+
+% authors d.mercier@mpie.de
+
+if strcmp(xAxisDirection, 'east') && strcmp(zAxisDirection, 'outOfPlane')
+    CoordSysVal = 1;
+elseif strcmp(xAxisDirection, 'north') && strcmp(zAxisDirection, 'outOfPlane')
+    CoordSysVal = 2;
+elseif strcmp(xAxisDirection, 'west') && strcmp(zAxisDirection, 'outOfPlane')
+    CoordSysVal = 3;
+elseif strcmp(xAxisDirection, 'south') && strcmp(zAxisDirection, 'outOfPlane')
+    CoordSysVal = 4;
+elseif strcmp(xAxisDirection, 'east') && strcmp(zAxisDirection, 'intoPlane')
+    CoordSysVal = 5;
+elseif strcmp(xAxisDirection, 'north') && strcmp(zAxisDirection, 'intoPlane')
+    CoordSysVal = 6;
+elseif strcmp(xAxisDirection, 'west') && strcmp(zAxisDirection, 'intoPlane')
+    CoordSysVal = 7;
+elseif strcmp(xAxisDirection, 'south') && strcmp(zAxisDirection, 'intoPlane')
+    CoordSysVal = 8;
+end
+
+end
