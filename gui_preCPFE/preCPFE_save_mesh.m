17f2e87754e1c70bb58bc17ab9b72815d6cb7168
unknown <d.mercier@mapc103.mpie.de>
2015-03-20 15:10:42 +0100

    improved function to save mesh configuration in a YAML file

--- gui_preCPFE/preCPFE_save_mesh.m
+++ gui_preCPFE/preCPFE_save_mesh.m
@@ -4,10 +4,15 @@ function preCPFE_save_mesh(mesh_to_save)
 % See in http://code.google.com/p/yamlmatlab/
 % mesh_to_save: Input variables (all mesh parameters to load) to save in a YAML config. file
 
-% authors: d.mercier@mpie.de
+% author: d.mercier@mpie.de
 
 [fname, fpath] = uiputfile('*.yaml', 'Save mesh in a YAML config. file as');
 
-WriteYaml(fullfile(fpath, fname), mesh_to_save);
+
+if isequal(fname, 0) || isequal(fpath, 0)
+    disp('User selected Cancel');
+else
+    WriteYaml(fullfile(fpath, fname), mesh_to_save);
+end
 
 end
