36f0abe3a3eff11cfe4c440b0d60bdcd8fd859af
unknown <d.mercier@mapc103.mpie.de>
2015-05-12 14:20:56 +0200

    updated MTEX function to plot IPF

--- MTEX_functions/MTEX_plotIPF.m
+++ MTEX_functions/MTEX_plotIPF.m
@@ -7,42 +7,47 @@ function MTEX_plotIPF(orientations, xaxis_alignement, varargin)
 
 % author: d.mercier@mpie.de
 
+no_orientation = 0;
+
 if nargin < 2
     xaxis_alignement = 1;
 end
 
 if nargin < 1
     warning_commwin('Wrong dataset for orientations calculation !');
+    no_orientation = 1;
 end
 
-%% Specify how to align the x-axis in plots
-switch(xaxis_alignement)
-    case {1}
-        plotx2east; %set the default plot direction of the xaxis
-    case {2}
-        plotx2north; %set the default plot direction of the xaxis
-    case {3}
-        plotx2south; %set the default plot direction of the xaxis
-    case {4}
-        plotx2west; %set the default plot direction of the xaxis
-    case {5}
-        plotzIntoPlane; %set the default plot direction of the zaxis
-    case {6}
-        plotzOutOfPlane; %set the default plot direction of the zaxis
+if ~no_orientation
+    %% Specify how to align the x-axis in plots
+    switch(xaxis_alignement)
+        case {1}
+            plotx2east; %set the default plot direction of the xaxis
+        case {2}
+            plotx2north; %set the default plot direction of the xaxis
+        case {3}
+            plotx2south; %set the default plot direction of the xaxis
+        case {4}
+            plotx2west; %set the default plot direction of the xaxis
+        case {5}
+            plotzIntoPlane; %set the default plot direction of the zaxis
+        case {6}
+            plotzOutOfPlane; %set the default plot direction of the zaxis
+    end
+    
+    %% Plot IPF for the bicrystal
+    v = vector3d(0, 0, 1); %zvector
+    figure('Name', 'IPF'); hold on;
+    
+    oM = ipdfHSVOrientationMapping(orientations);
+    plot(oM); hold on;
+    
+    plotIPDF(orientations, v, ...
+        'markerSize', 5, ...
+        'points', length(orientations), ...
+        'marker', 'o', ...
+        'markerfacecolor', 'none', ...
+        'markeredgecolor', 'k');
 end
-
-%% Plot IPF for the bicrystal
-v = vector3d(0, 0, 1); %zvector
-figure('Name', 'IPF'); hold on;
-
-oM = ipdfHSVOrientationMapping(orientations);
-plot(oM); hold on;
-
-plotIPDF(orientations, v, ...
-    'markerSize', 3, ...
-    'points', 500, ...
-    'marker', 'o', ...
-    'markerfacecolor', 'none', ...
-    'markeredgecolor', 'k');
 
 end
