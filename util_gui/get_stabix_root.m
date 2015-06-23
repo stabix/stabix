ae92c051bce1d1f40fc339d3d746af2116f01a5c
david mercier <d.mercier@mpie.de>
2015-06-23 19:03:43 +0200

    improved function to get_stabix_root (errordlg --> error in commandwindow)

--- util_gui/get_stabix_root.m
+++ util_gui/get_stabix_root.m
@@ -8,7 +8,9 @@ stabix_root = getenv('SLIP_TRANSFER_TBX_ROOT');
 
 if isempty(stabix_root)
     msg = 'Run the path_management.m script !';
-    errordlg(msg, 'File Error');
+    commandwindow;
+    display(msg);
+    %errordlg(msg, 'File Error');
     error(msg);
 end
 
