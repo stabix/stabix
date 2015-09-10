dee69d3e75d28fce5f5d3649bcb6a0884ec4bdba
david mercier <d.mercier@mpie.de>
2015-09-10 18:21:32 +0200

    add some check for MTEX functions when the EBSD dataset is not from a single phase material

--- mtexFunctions/mtex_convert2TSLdata.m
+++ mtexFunctions/mtex_convert2TSLdata.m
@@ -19,51 +19,55 @@ if nargin < 1
     import_wizard;
 end
 
-%% Grains definition
-grains = calcGrains(ebsd, 'angle', angle_value * degree);
-% subSet function to get only 1 grain
-
-% grains = smooth(grains,iter,..,param,val,..);
-
-%% Initialization of path
-fpath = get_stabix_root;
-
-%% Grain File type 2
-fname_GF2 = 'importEBSDdata_frommtex_GF2.txt';
-
-fdata.title = 'EBSD data imported using import_wizard from MTEX toolbox';
-fdata.number_of_grains = grains.size(1);
-fdata.eul_ang = (grains.meanOrientation.Euler) * 180/pi;
-grain_position = grains.centroid;
-fdata.x_positions = grain_position(:,1);
-fdata.y_positions = grain_position(:,2);
-fdata.phase = nonzeros(grains.phaseId);
-fdata.grain_diameter = ((grains.area/pi).^0.5)./2;
-
-% Write TSL-OIM grain file type 2
-write_oim_grain_file_type2(fdata, fpath, fname_GF2);
-
-%% Reconstructed Boundaries file
-fname_RB = 'importEBSDdata_frommtex_RB.txt';
-
-%p = patch('Faces',grains.boundary.F,'Vertices',grains.boundary.V,'FaceColor','w');
-%fdata.number_of_grain_boundaries = size(grains.boundary.F,1);
-
-[grains_x, grains_y] = centroid(grains);
-
-[fdata.GBvx, ...
-    fdata.GBvy, ...
-    fdata.GB2cells, ...
-    fdata.flag_VorFailed] = ...
-    neighbooring_edge_of_2cells(...
-    grains_x', grains_y');
-
-fdata.GBvx = fdata.GBvx';
-fdata.GBvy = fdata.GBvy';
-fdata.GB2cells = fdata.GB2cells';
-fdata.number_of_grain_boundaries = size(fdata.GBvx, 1);
-
-% Write TSL-OIM grain file type 2
-write_oim_reconstructed_boundaries_file(fdata, fpath, fname_RB);
+if max(gui.ebsdMTEX.phase) == 1
+    %% Grains definition
+    grains = calcGrains(ebsd, 'angle', angle_value * degree);
+    % subSet function to get only 1 grain
+    
+    % grains = smooth(grains,iter,..,param,val,..);
+    
+    %% Initialization of path
+    fpath = get_stabix_root;
+    
+    %% Grain File type 2
+    fname_GF2 = 'importEBSDdata_frommtex_GF2.txt';
+    
+    fdata.title = 'EBSD data imported using import_wizard from MTEX toolbox';
+    fdata.number_of_grains = grains.size(1);
+    fdata.eul_ang = (grains.meanOrientation.Euler) * 180/pi;
+    grain_position = grains.centroid;
+    fdata.x_positions = grain_position(:,1);
+    fdata.y_positions = grain_position(:,2);
+    fdata.phase = nonzeros(grains.phaseId);
+    fdata.grain_diameter = ((grains.area/pi).^0.5)./2;
+    
+    % Write TSL-OIM grain file type 2
+    write_oim_grain_file_type2(fdata, fpath, fname_GF2);
+    
+    %% Reconstructed Boundaries file
+    fname_RB = 'importEBSDdata_frommtex_RB.txt';
+    
+    %p = patch('Faces',grains.boundary.F,'Vertices',grains.boundary.V,'FaceColor','w');
+    %fdata.number_of_grain_boundaries = size(grains.boundary.F,1);
+    
+    [grains_x, grains_y] = centroid(grains);
+    
+    [fdata.GBvx, ...
+        fdata.GBvy, ...
+        fdata.GB2cells, ...
+        fdata.flag_VorFailed] = ...
+        neighbooring_edge_of_2cells(...
+        grains_x', grains_y');
+    
+    fdata.GBvx = fdata.GBvx';
+    fdata.GBvy = fdata.GBvy';
+    fdata.GB2cells = fdata.GB2cells';
+    fdata.number_of_grain_boundaries = size(fdata.GBvx, 1);
+    
+    % Write TSL-OIM grain file type 2
+    write_oim_reconstructed_boundaries_file(fdata, fpath, fname_RB);
+else
+    display('Only permitted for a single phase!');
+end
 
 end
--- mtexFunctions/mtex_setEBSDdata.m
+++ mtexFunctions/mtex_setEBSDdata.m
@@ -9,37 +9,41 @@ gui = guidata(gcf);
 
 mtex_env = getMTEXpref;
 
-[fpath, fname_GF2, fname_RB] = mtex_convert2TSLdata(gui.ebsdMTEX);
-
-% Set paths in the GUI
-set(gui.handles.FileGF2, 'String', fullfile(fpath, fname_GF2));
-set(gui.handles.FileRB, 'String', fullfile(fpath, fname_RB));
-gui.config_map.filename_grain_file_type2 = fname_GF2;
-gui.config_map.filename_reconstructed_boundaries_file = fname_RB;
-gui.config_map.pathname_grain_file_type2 = fpath;
-gui.config_map.pathname_reconstructed_boundaries_file = fpath;
-gui.flag.newDataFlag = 1;
-
-% Set coordinate sytem
-CoordSysVal = mtex_setCoordSys(mtex_env.xAxisDirection, ...
-    mtex_env.zAxisDirection);
-set(gui.handles.pmcoordsyst, 'Value', CoordSysVal);
-
-% Set scan unit
-if strcmp(gui.ebsdMTEX.scanUnit, 'nm')
-    set(gui.handles.pm_unit, 'Value', 1);
-elseif strcmp(gui.ebsdMTEX.scanUnit, 'um')
-    set(gui.handles.pm_unit, 'Value', 2);
-elseif strcmp(gui.ebsdMTEX.scanUnit, 'mm')
-    set(gui.handles.pm_unit, 'Value', 3);
+if max(gui.ebsdMTEX.phase) == 1
+    [fpath, fname_GF2, fname_RB] = mtex_convert2TSLdata(gui.ebsdMTEX);
+    
+    % Set paths in the GUI
+    set(gui.handles.FileGF2, 'String', fullfile(fpath, fname_GF2));
+    set(gui.handles.FileRB, 'String', fullfile(fpath, fname_RB));
+    gui.config_map.filename_grain_file_type2 = fname_GF2;
+    gui.config_map.filename_reconstructed_boundaries_file = fname_RB;
+    gui.config_map.pathname_grain_file_type2 = fpath;
+    gui.config_map.pathname_reconstructed_boundaries_file = fpath;
+    gui.flag.newDataFlag = 1;
+    
+    % Set coordinate sytem
+    CoordSysVal = mtex_setCoordSys(mtex_env.xAxisDirection, ...
+        mtex_env.zAxisDirection);
+    set(gui.handles.pmcoordsyst, 'Value', CoordSysVal);
+    
+    % Set scan unit
+    if strcmp(gui.ebsdMTEX.scanUnit, 'nm')
+        set(gui.handles.pm_unit, 'Value', 1);
+    elseif strcmp(gui.ebsdMTEX.scanUnit, 'um')
+        set(gui.handles.pm_unit, 'Value', 2);
+    elseif strcmp(gui.ebsdMTEX.scanUnit, 'mm')
+        set(gui.handles.pm_unit, 'Value', 3);
+    else
+        warning('Can''t set the scan unit from imported EBSD data...');
+    end
+    
+    % Encapsulation of data in the GUI
+    guidata(gcf, gui);
+    
+    % Run the plot of the loaded EBSD data
+    interface_map_set_coordinate_convention;
 else
-    warning('Can''t set the scan unit from imported EBSD data...');
+    display('Only permitted for a single phase!');
 end
-
-% Encapsulation of data in the GUI
-guidata(gcf, gui);
-
-% Run the plot of the loaded EBSD data
-interface_map_set_coordinate_convention;
 
 end
