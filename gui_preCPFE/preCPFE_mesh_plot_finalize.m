c5575617c32698bd8c090629ef9be22c5410f417
unknown <d.mercier@mapc103.mpie.de>
2015-10-06 16:13:17 +0200

    improved script to plot xyzlabel to a function with LaTeX interpreter as an option

--- gui_preCPFE/preCPFE_mesh_plot_finalize.m
+++ gui_preCPFE/preCPFE_mesh_plot_finalize.m
@@ -1,6 +1,7 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
 function preCPFE_mesh_plot_finalize
 %% Axis setting
+% author: c.zambaldi@mpie.de
 
 axis tight; % Axis tight to the sample
 axis equal; % Axis aspect ratio
--- util/xyzlabel.m
+++ util/xyzlabel.m
@@ -1,4 +1,25 @@
 % Copyright 2013 Max-Planck-Institut für Eisenforschung GmbH
-xlabel('X');
-ylabel('Y');
-zlabel('Z');
+function xyzlabel(Xstring, Ystring, Zstring, interpreter, varargin)
+%% Axis setting
+% author: d.mercier@mpie.de
+
+if nargin < 3
+    %interpreter = 'none';
+    interpreter = 'latex';
+end
+if nargin < 3
+    Zstring = 'Z';
+end
+if nargin < 2
+    Ystring = 'Y';
+end
+if nargin < 1
+    Xstring = 'X';
+end
+
+xLabel = xlabel(Xstring);
+yLabel = ylabel(Ystring);
+zLabel = zlabel(Zstring);
+set([xLabel, yLabel, zLabel], 'Interpreter', interpreter);
+
+end
